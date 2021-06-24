82c0c61613ddd7b58ff40867dc2a61e36db56828
kimmyunghoon <roykimmyunghoon@gmail.com>
2021-06-25 01:59:40 +0900

    t

--- src/pages/DjangoTodo.js
+++ src/pages/DjangoTodo.js
@@ -1,35 +1,70 @@
+import axios from 'axios';
 import React, { Component } from 'react'
 import '../css/App.css';
+import CustomModal from './todocomponents/Modal';
 
-const tasks = [
-    {
-        "id": 1,
-        "title": "shopping",
-        "description": "shopping for computer",
-        "completed": false
-    },
-    {
-        "id": 2,
-        "title": "play music11",
-        "description": "gogo",
-        "completed": true
-    },
-    {
-        "id": 3,
-        "title": "play music22",
-        "description": "gogo",
-        "completed": false
-    }
-]
 
 export class DjangoTodo extends Component {
     constructor(props){
         super(props);
         this.state = {
+            modal : false,
             viewCompleted : false,
-            taskList : tasks
+            activeItem : {
+                title: "",
+                description : "",
+                completed : false,
+            },
+            todoList : [],
         }
+        
     }
+
+    componentDidMount() {
+        this.refreshList();
+    }
+
+    refreshList = () => {
+        axios
+            .get("http://localhost:8000/api/tasks/")
+            .then(res => this.setState({ todoList : res.data }))
+            .catch(err => console.log(err))
+    }
+
+
+    // 토글 프로퍼티 만들기
+    toggle = () => {
+        this.setState({modal : !this.state.modal});
+    }
+
+    handleSubmit = (item) => {
+        this.toggle();
+        if(item.id) {
+            axios
+                .put(`http://localhost:8000/api/tasks/${item.id}/`, item)
+                .then(res => this.refreshList())
+        }
+        axios
+            .post("http://localhost:8000/api/tasks/", item)
+            .then(res => this.refreshList())
+
+    }
+
+    handleDelete = (item) => {
+        axios
+            .delete(`http://localhost:8000/api/tasks/${item.id}/`)
+            .then(res => this.refreshList());
+    }
+
+    createItem = () => {
+        const item = { title : "", modal :!this.state.modal };
+        this.setState({ activeItem: item, modal: !this.state.modal });
+    }
+
+    editItem = item => {
+        this.setState({ activeItem : item, modal : !this.state.modal })
+    }
+
 
     displayCompleted = status => {
         if(status) {
@@ -42,13 +77,13 @@ export class DjangoTodo extends Component {
         return (
             <div className="my-5 tab-list">
                 <span
-                    onclick={() => this.displayCompleted(true)}
+                    onClick={() => this.displayCompleted(true)}
                     className={this.state.viewCompleted ? "active" : ""}
                 >
                 completed
                 </span>
                 <span
-                    onclick={() => this.displayCompleted(false)}
+                    onClick={() => this.displayCompleted(false)}
                     className={this.state.viewCompleted ? "active" : ""}
                 >
                 Incompleted
@@ -60,7 +95,7 @@ export class DjangoTodo extends Component {
     // rendering items in the list (complete || incomplete)
     renderItems = () => {
         const { viewCompleted } = this.state;
-        const newItems = this.state.taskList.filter(
+        const newItems = this.state.todoList.filter(
             item => item.completed == viewCompleted
         );
 
@@ -68,26 +103,28 @@ export class DjangoTodo extends Component {
             <li key={item.id} 
                 className="list-group-item d-flex justify-content-between align-items-center"
             >
-                <span className="">
+                <span className={`todo-title mr-2 ${this.state.viewCompleted ? "completed-todo" : ""}`}
+                    title={item.title}>
                     {item.title}
+                </span>
+                <span>
+                    <button className="btn btn-info mr-2" onClick={this.editItem}>Edit</button>
+                    <button className="btn btn-danger mr-2">Delete</button>
                 </span>
             </li>
         ))
     };
 
     
-
-
-
     render() {
         return (
-            <main className="context">
-                <h1 className="text-black text-uppercase text-center my-4"> Task Manager </h1>
-                <h1 className="row">
+            <main className="content p-3 mb-2 bg-info">
+                <h1 className="text-white text-uppercase text-center my-4"> Task Manager </h1>
+                <div className="row">
                     <div className="col-md-6 col-sm-10 mx-auto p-0">
                         <div className="card p-3">
                             <div>
-                                <button className="btn btn-warning">Add Task</button>
+                                <button className="btn btn-warning" onClick={this.createItem}>Add Task</button>
                             </div>
                             {this.renderTabList()}
                             <ul className="list-group list-group-flush">
@@ -96,7 +133,15 @@ export class DjangoTodo extends Component {
 
                         </div>
                     </div>
-                </h1>
+                </div>
+                <footer className="my-3 mb-2 bg-info text-white text-center">Copyright &copy; All Rights Reserved</footer>
+                {this.state.modal ? (
+                    <CustomModal 
+                        activeItem={this.state.activeItem}
+                        toggle={this.toggle}
+                        onSave={this.handleSubmit}
+                    />
+                ) : null }
             </main>
         )
     }
